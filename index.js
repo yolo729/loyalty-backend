0102ed024df34bc1d133f211d9978ba5061fbad7
Artem Milevskyi <milevskyi.a@gmail.com>
2024-05-07 23:12:32 +0200

    Fixed some problems

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import db from "./config/Database.js";
 import router from "./routes/index.js";
+
+const apiUrl = process.env.MY_DOMAIN;
+const Port = process.env.PORT;
+const ENV = process.env.ENV;
+
 dotenv.config();
 const app = express();
 
@@ -14,9 +19,12 @@ try {
     console.error(error);
 }
 
-app.use(cors({ credentials:true, origin:'http://localhost:5173', methods: ['GET', 'POST', 'PUT', 'DELETE'] }));
+app.use(cors({ credentials:true, origin: {apiUrl}, methods: ['GET', 'POST', 'PUT', 'DELETE'] }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
-app.listen(5000, ()=> console.log('Server running at port 5000'));
+app.listen(Port, ENV == "dev" ? "127.0.0.1" : "0.0.0.0", ()=> console.log('Server running at port 5000'));
+
+
+  
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ import { refreshToken } from "../controllers/RefreshToken.js";
 
 const router = express.Router();
 
-router.get('/users', verifyToken, getUsers);
 router.post('/users', Register);
 router.post('/login', Login);
+router.get('/users', getUsers);
 router.get('/token', refreshToken);
 router.delete('/logout', Logout);
 
