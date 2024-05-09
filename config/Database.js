d1b6e9a6c41117e334b1ddead9c0e4519ec8cbce
Artem Milevskyi <milevskyi.a@gmail.com>
2024-05-10 02:13:38 +1000

    added dotenv modules

--- config/Database.js
+++ config/Database.js
@@ -1,12 +1,13 @@
 
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
+import 'dotenv/config'
 
-const DB_Name = process.env.DB_NAME 
-const ROOT = process.env.ROOT 
-const PWD = process.env.PASSWORD 
-const HOST = process.env.HOST 
+const DB_Name = process.env.DB_NAME
+const ROOT = process.env.ROOT
+const PWD = process.env.PASSWORD
+const HOST = process.env.HOST
 
-const db = new Sequelize(DB_Name,ROOT,PWD,{
+const db = new Sequelize(DB_Name, ROOT, PWD, {
     host: HOST,
     dialect: "mysql"
 });
